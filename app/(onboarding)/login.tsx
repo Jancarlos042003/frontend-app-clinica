7ea7a50420deddd1feb36c288fbc933beeee8698
Jancarlos042003 <U21302940@utp.edu.pe>
2025-05-25 17:28:09 -0500

    Wrap login screen in DismissKeyboardView

--- app/(onboarding)/login.tsx
+++ app/(onboarding)/login.tsx
@@ -8,6 +8,7 @@ import RegisterSection from '../../components/auth/RegisterSection';
 import BackButton from '../../components/buttons/BackButton';
 import SubmitButton from '../../components/buttons/SubmitButton';
 import TogglePasswordButton from '../../components/buttons/TogglePasswordButton';
+import DismissKeyboardView from '../../components/layouts/DismissKeyboardView';
 import HeaderBackground from '../../components/layouts/HeaderBackground';
 import { API_URL } from '../../config/env';
 import useApi from '../../hooks/useApi';
@@ -50,77 +51,79 @@ const Login = () => {
 
   return (
     <SafeAreaView className="flex-1 bg-[#EDEFFC]">
-      <HeaderBackground />
+      <DismissKeyboardView>
+        <HeaderBackground />
 
-      <View className="flex-1">
-        <BackButton onPress={router.back} />
+        <View className="flex-1">
+          <BackButton onPress={router.back} />
 
-        <View className="mt-[365px] flex-1 rounded-t-3xl bg-primary_100 px-7 py-9">
-          <Text className="text-4xl font-bold text-primary">Iniciar Sesión</Text>
+          <View className="mt-[365px] flex-1 rounded-t-3xl bg-primary_100 px-7 py-9">
+            <Text className="text-4xl font-bold text-primary">Iniciar Sesión</Text>
 
-          <Text className="mb-4 text-base text-black">Ingresa tus credenciales</Text>
+            <Text className="mb-4 text-base text-black">Ingresa tus credenciales</Text>
 
-          <View className="w-full">
-            <Controller
-              control={control}
-              name="dni"
-              render={({ field: { onChange, onBlur, value } }) => (
-                <View className="mb-2">
-                  <TextInput
-                    className={`border bg-white ${errors.dni ? 'border-red-500' : 'border-[#D4D4D8]'} mb-2 rounded-md p-4 text-lg text-[#101010] focus:border-primary`}
-                    placeholder="DNI"
-                    keyboardType="number-pad"
-                    maxLength={8}
-                    value={value}
-                    onChangeText={onChange}
-                    onBlur={onBlur}
-                  />
-                  {errors.dni && (
-                    <Text className="mb-4 text-sm text-red-500">{errors.dni.message}</Text>
-                  )}
-                </View>
-              )}
-            />
-
-            <Controller
-              control={control}
-              name="password"
-              render={({ field: { onChange, onBlur, value } }) => (
-                <View className="mb-1">
-                  <View className="relative">
+            <View className="w-full">
+              <Controller
+                control={control}
+                name="dni"
+                render={({ field: { onChange, onBlur, value } }) => (
+                  <View className="mb-2">
                     <TextInput
-                      className={`border bg-white ${errors.password ? 'border-red-500' : 'border-[#D4D4D8]'} mb-2 rounded-md p-4 pr-12 text-lg text-[#101010] focus:border-primary`}
-                      placeholder="Contraseña"
-                      secureTextEntry={!showPassword}
+                      className={`border bg-white ${errors.dni ? 'border-red-500' : 'border-[#D4D4D8]'} mb-2 rounded-md p-4 text-lg text-[#101010] focus:border-primary`}
+                      placeholder="DNI"
+                      keyboardType="number-pad"
+                      maxLength={8}
                       value={value}
-                      onChangeText={(text) => {
-                        onChange(text);
-                        clearError();
-                      }}
+                      onChangeText={onChange}
                       onBlur={onBlur}
                     />
-                    <TogglePasswordButton
-                      showPassword={showPassword}
-                      onToggle={() => setShowPassword(!showPassword)}
-                    />
+                    {errors.dni && (
+                      <Text className="mb-4 text-sm text-red-500">{errors.dni.message}</Text>
+                    )}
                   </View>
-                  {errors.password && (
-                    <Text className="mb-4 text-sm text-red-500">{errors.password.message}</Text>
-                  )}
-                  <Pressable className="mb-4 self-end">
-                    <Text className="font-semibold text-primary">¿Olvidaste tu contraseña?</Text>
-                  </Pressable>
-                </View>
-              )}
-            />
-            <SubmitButton onPress={handleSubmit(onSubmit)} loading={loading} />
+                )}
+              />
 
-            {error && <Text className="mt-1 text-center text-sm text-red-500">{error}</Text>}
+              <Controller
+                control={control}
+                name="password"
+                render={({ field: { onChange, onBlur, value } }) => (
+                  <View className="mb-1">
+                    <View className="relative">
+                      <TextInput
+                        className={`border bg-white ${errors.password ? 'border-red-500' : 'border-[#D4D4D8]'} mb-2 rounded-md p-4 pr-12 text-lg text-[#101010] focus:border-primary`}
+                        placeholder="Contraseña"
+                        secureTextEntry={!showPassword}
+                        value={value}
+                        onChangeText={(text) => {
+                          onChange(text);
+                          clearError();
+                        }}
+                        onBlur={onBlur}
+                      />
+                      <TogglePasswordButton
+                        showPassword={showPassword}
+                        onToggle={() => setShowPassword(!showPassword)}
+                      />
+                    </View>
+                    {errors.password && (
+                      <Text className="mb-4 text-sm text-red-500">{errors.password.message}</Text>
+                    )}
+                    <Pressable className="mb-4 self-end">
+                      <Text className="font-semibold text-primary">¿Olvidaste tu contraseña?</Text>
+                    </Pressable>
+                  </View>
+                )}
+              />
+              <SubmitButton onPress={handleSubmit(onSubmit)} loading={loading} />
 
-            <RegisterSection onPress={() => router.push('verify-dni')} />
+              {error && <Text className="mt-1 text-center text-sm text-red-500">{error}</Text>}
+
+              <RegisterSection onPress={() => router.push('verify-dni')} />
+            </View>
           </View>
         </View>
-      </View>
+      </DismissKeyboardView>
     </SafeAreaView>
   );
 };
