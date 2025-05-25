6d9ebde3035029aad8d1054ce68cf47492aa84c6
Jancarlos042003 <U21302940@utp.edu.pe>
2025-05-25 04:14:01 -0500

    Extract reusable components

---
+++ components/buttons/SubmitButton.tsx
@@ -0,0 +1,30 @@
+import { ActivityIndicator, Pressable, Text } from 'react-native';
+
+interface SubmitButtonProps {
+  onPress: () => void;
+  loading?: boolean;
+  text?: string;
+  className?: string;
+}
+
+const SubmitButton = ({
+  onPress,
+  loading = false,
+  text = 'Iniciar sesión',
+  className = '',
+}: SubmitButtonProps) => {
+  return (
+    <Pressable
+      className={`items-center rounded-lg py-5 ${loading ? 'bg-primary_300' : 'bg-primary'} ${className}`}
+      onPress={onPress}
+      disabled={loading}>
+      {loading ? (
+        <ActivityIndicator color="#65a5cb" />
+      ) : (
+        <Text className="text-lg font-bold text-white">{text}</Text>
+      )}
+    </Pressable>
+  );
+};
+
+export default SubmitButton;
---
+++ components/layouts/HeaderBackground.tsx
@@ -0,0 +1,27 @@
+import { ReactNode } from 'react';
+import { ImageBackground, ImageSourcePropType, View } from 'react-native';
+
+import Logo from '../icons/Logo';
+
+type HeaderBackgroundProps = {
+  image?: ImageSourcePropType;
+  children?: ReactNode;
+};
+
+const imageDefault = require('../../assets/images/login-med.png'); // Default image if none is provided
+
+const HeaderBackground = ({
+  image = imageDefault,
+  children = <Logo />, // Default children if none are provided
+}: HeaderBackgroundProps) => {
+  return (
+    <View className="absolute left-0 right-0 top-0 h-2/4 w-full">
+      <ImageBackground source={image} className="flex-1" resizeMode="cover">
+        <View className="absolute inset-0 bg-black/45" />
+        <View className="flex-1 items-center justify-center">{children}</View>
+      </ImageBackground>
+    </View>
+  );
+};
+
+export default HeaderBackground;
