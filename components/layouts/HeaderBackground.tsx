3ca58db6b0987aa1e9329e5f8a1d307a94da7859
Jancarlos042003 <U21302940@utp.edu.pe>
2025-05-27 15:52:22 -0500

    Update HeaderBackground component to use fixed height for improved layout

--- components/layouts/HeaderBackground.tsx
+++ components/layouts/HeaderBackground.tsx
@@ -15,7 +15,7 @@ const HeaderBackground = ({
   children = <Logo />, // Default children if none are provided
 }: HeaderBackgroundProps) => {
   return (
-    <View className="absolute left-0 right-0 top-0 h-2/4 w-full">
+    <View className="absolute left-0 right-0 top-0 h-[390px] w-full">
       <ImageBackground source={image} className="flex-1" resizeMode="cover">
         <View className="absolute inset-0 bg-black/45" />
         <View className="flex-1 items-center justify-center">{children}</View>
