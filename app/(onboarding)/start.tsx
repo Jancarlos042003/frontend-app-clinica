9e376ef579e323ef853dfcf0d6f55dc4c58e993a
Jancarlos042003 <U21302940@utp.edu.pe>
2025-05-28 00:29:14 -0500

    Removing the className in LinearGradient

--- app/(onboarding)/start.tsx
+++ app/(onboarding)/start.tsx
@@ -1,11 +1,11 @@
 import { LinearGradient } from 'expo-linear-gradient';
 import { useRouter } from 'expo-router';
 import { ImageBackground, StatusBar, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 import RegisterSection from '../../components/auth/RegisterSection';
 import SubmitButton from '../../components/buttons/SubmitButton';
 import Logo from '../../components/icons/Logo';
-import { ScreenWrapper } from '../../components/layouts/ScreenWrapper';
 
 const img_presentation = require('../../assets/images/start-presentacion.jpg');
 
@@ -13,10 +13,10 @@ const Start = () => {
   const router = useRouter();
 
   return (
-    <ScreenWrapper edges={['bottom']}>
+    <SafeAreaView className="flex-1" edges={['bottom']}>
       <StatusBar barStyle="light-content" backgroundColor="transparent" />
       <ImageBackground source={img_presentation} className="flex-1" resizeMode="cover">
-        <LinearGradient colors={['rgba(41,75,110,0.3)', 'rgba(27,62,89,0.3)']} className="flex-1">
+        <LinearGradient colors={['rgba(41,75,110,0.3)', 'rgba(27,62,89,0.3)']} style={{ flex: 1 }}>
           <View className="flex-1 justify-between px-8 py-20">
             <Logo />
 
@@ -32,7 +32,7 @@ const Start = () => {
           </View>
         </LinearGradient>
       </ImageBackground>
-    </ScreenWrapper>
+    </SafeAreaView>
   );
 };
 
