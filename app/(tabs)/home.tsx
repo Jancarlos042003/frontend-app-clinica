a3f1f1a4d51d96c6dd121023d6461e27912b85a5
Jancarlos042003 <U21302940@utp.edu.pe>
2025-06-03 19:05:03 -0500

    Display welcome message

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,9 +1,15 @@
 import { Text, View } from 'react-native';
 
+import { useUser } from '../../hooks/useUser';
+
 const Home = () => {
+  const { user } = useUser();
+
   return (
-    <View className="flex-1 bg-primary">
-      <Text>Screen Home</Text>
+    <View className="flex-1">
+      <Text className="text-2xl">
+        Bienvenido <Text className="font-bold">{user?.name}</Text>
+      </Text>
     </View>
   );
 };
