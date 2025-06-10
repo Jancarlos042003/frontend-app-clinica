50cac9c9f74b0708e254032840833b8c120cd197
Jancarlos042003 <U21302940@utp.edu.pe>
2025-06-10 01:50:18 -0500

    Improve token verification and refresh in the authentication flow

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,61 @@
-import Start from './(onboarding)/start';
 import '../global.css';
+import axios from 'axios';
+import { useRouter } from 'expo-router';
+import { useEffect } from 'react';
+import { ActivityIndicator, View } from 'react-native';
+
+import { getAccessToken, getRefreshToken, saveTokens } from '../auth/tokenService';
+import { API_URL } from '../config/env';
+import { useUser } from '../hooks/useUser';
+import { isTokenValid } from '../utils/auth/jwt';
 
 const Index = () => {
-  return <Start />;
+  const router = useRouter();
+  const { loginUser } = useUser();
+
+  useEffect(() => {
+    const checkAuthStatus = async () => {
+      try {
+        const token = await getAccessToken();
+        if (token && isTokenValid(token)) {
+          // Si hay un token, intenta iniciar sesión al usuario
+          loginUser(token);
+          router.replace('/(tabs)/home');
+        } else {
+          // Si hay token pero este esta vencido
+          const refreshToken = await getRefreshToken();
+
+          if (refreshToken) {
+            const response = await axios.post(`${API_URL}/api/auth/refresh-token`, {
+              refreshToken,
+            });
+
+            if (response.data) {
+              await saveTokens(response.data.token, response.data.refreshToken);
+              loginUser(response.data.token);
+              router.replace('/(tabs)/home');
+              return;
+            }
+          }
+
+          // Si no hay token, redirige a la pantalla de inicio
+          router.replace('/(onboarding)/start');
+        }
+      } catch (error) {
+        console.error('Error al verificar el estado de autenticación:', error);
+        // En caso de error, redirige a la pantalla de inicio
+        router.replace('/(onboarding)/start');
+      }
+    };
+
+    checkAuthStatus();
+  }, []);
+
+  return (
+    <View className="flex-1 items-center justify-center">
+      <ActivityIndicator size="large" color="#0000ff" />
+    </View>
+  );
 };
 
 export default Index;
