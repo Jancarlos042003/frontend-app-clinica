c735c3411e7ea7da63d2547e3c255017049a63b7
Jancarlos042003 <U21302940@utp.edu.pe>
2025-06-10 02:13:18 -0500

    Refactor home component

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,15 +1,9 @@
 import { Text, View } from 'react-native';
 
-import { useUser } from '../../hooks/useUser';
-
 const Home = () => {
-  const { user } = useUser();
-
   return (
     <View className="flex-1">
-      <Text className="text-2xl">
-        Bienvenido <Text className="font-bold">{user?.name}</Text>
-      </Text>
+      <Text>Página de inicio</Text>
     </View>
   );
 };
