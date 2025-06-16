978374dfb11d124b68b30e86b61a3f5a84918cfa
Jancarlos042003 <U21302940@utp.edu.pe>
2025-06-15 20:39:50 -0500

    Refactor ScreenWrapper component to use StyleSheet for styling

--- components/layouts/ScreenWrapper.tsx
+++ components/layouts/ScreenWrapper.tsx
@@ -1,20 +1,23 @@
 import { ReactNode } from 'react';
+import { StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 type ScreenWrapperProps = {
   children: ReactNode;
   edges?: ('top' | 'bottom' | 'left' | 'right')[];
-  className?: string;
 };
 
-export function ScreenWrapper({
-  children,
-  edges = ['top', 'bottom'],
-  className,
-}: ScreenWrapperProps) {
+export function ScreenWrapper({ children, edges = ['top', 'bottom'] }: ScreenWrapperProps) {
   return (
-    <SafeAreaView className={`flex-1 bg-primary_100 ${className}`} edges={edges}>
+    <SafeAreaView style={styles.wrapper} edges={edges}>
       {children}
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    backgroundColor: '#F3F7FA',
+  },
+});
