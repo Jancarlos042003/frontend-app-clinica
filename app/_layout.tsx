9cb7750136b991d782c167397de70e2662920a23
Jancarlos Tuñoque <u21302940@utp.edu.pe>
2025-06-19 05:11:24 -0500

    Add UserProvider and PaperProvider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,22 @@
 import { Stack } from 'expo-router';
-import { StatusBar } from 'react-native';
+import { PaperProvider } from 'react-native-paper';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+import { UserProvider } from '../context/UserContext';
 
 // Layout raíz de la app con un Stack Navigator
 export default function RootLayout() {
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent />
-      <Stack
-        screenOptions={{
-          headerShown: false,
-        }}
-      />
-    </SafeAreaProvider>
+    <UserProvider>
+      <PaperProvider>
+        <SafeAreaProvider>
+          <Stack
+            screenOptions={{
+              headerShown: false,
+            }}
+          />
+        </SafeAreaProvider>
+      </PaperProvider>
+    </UserProvider>
   );
 }
