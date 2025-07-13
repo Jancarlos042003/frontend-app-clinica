6232831fca933cd9485cae9fe6a991f90cf1f568
Jancarlos Tuñoque <u21302940@utp.edu.pe>
2025-07-12 21:47:14 -0500

    Agregar pantalla SOS con diseño básico

---
+++ app/(tabs)/sos.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const Sos = () => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>SOS Screen</Text>
+    </View>
+  );
+};
+
+export default Sos;
