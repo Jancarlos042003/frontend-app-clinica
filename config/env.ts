907e0a9330c7ff659ea34be452d3066e4608d69a
Jancarlos Tuñoque <u21302940@utp.edu.pe>
2025-07-13 21:18:11 -0500

    Actualizar la exportación de API_URL para utilizar la variable de entorno EXPO_PUBLIC_API_URL en lugar de Constants.expoConfig.

--- config/env.ts
+++ config/env.ts
@@ -1,7 +1,1 @@
-import Constants from 'expo-constants';
-
-// Obtiene las variables de entorno adicionales (definidas como extra en app.config.js) del objeto Constants.expoConfig.
-const raw = Constants.expoConfig?.extra as { API_URL: string }; // Hace un typecasting de esas variables a un tipo con la propiedad API_URL de tipo string.
-
-// Exporta la variable API_URL para que pueda ser utilizada en toda la aplicación.
-export const API_URL = raw.API_URL;
+export const API_URL = process.env.EXPO_PUBLIC_API_URL;
