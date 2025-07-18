daf56ac5a876f95030c226b42db5aba8394fe8bf
Jancarlos Tuñoque <u21302940@utp.edu.pe>
2025-07-18 08:09:42 -0500

    Eliminar la pantalla de tratamientos médicos del layout de salud

--- app/(tabs)/health/_layout.tsx
+++ app/(tabs)/health/_layout.tsx
@@ -1,6 +1,5 @@
 import { Stack } from 'expo-router';
 import React from 'react';
-import { Platform } from 'react-native';
 
 const HealthLayout = () => {
   return (
@@ -9,17 +8,6 @@ const HealthLayout = () => {
         name="index"
         options={{
           headerShown: false,
-        }}
-      />
-
-      <Stack.Screen
-        name="treatments/index"
-        options={{
-          headerTitle: 'Tratamientos Médicos',
-          headerStyle: { backgroundColor: '#32729F' },
-          headerShadowVisible: false,
-          headerTintColor: '#fff',
-          ...(Platform.OS === 'ios' ? { animation: 'default' } : { animation: 'slide_from_right' }),
         }}
       />
     </Stack>
