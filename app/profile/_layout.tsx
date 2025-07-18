b48b79a900332fa68900d877788fe56c50b575d7
Jancarlos Tuñoque <u21302940@utp.edu.pe>
2025-07-18 08:55:28 -0500

    Eliminar componentes de pantalla modal en ProfileLayout

--- app/profile/_layout.tsx
+++ app/profile/_layout.tsx
@@ -1,8 +1,6 @@
 import { Stack } from 'expo-router';
 import { Platform } from 'react-native';
 
-import HeaderBackgroundWithBorder from '../../components/headers/HeaderBackgroundWithBorder';
-
 export default function ProfileLayout() {
   return (
     <Stack>
@@ -14,28 +12,6 @@ export default function ProfileLayout() {
           headerTitleStyle: { color: '#fff', fontWeight: 'bold', fontSize: 19 },
           headerTintColor: '#fff', // Define el color del icono de retroceso
           ...(Platform.OS === 'ios' ? { animation: 'default' } : { animation: 'slide_from_right' }),
-        }}
-      />
-
-      <Stack.Screen
-        name="modal-edit-phone"
-        options={{
-          title: 'Editar teléfono',
-          presentation: 'modal',
-          headerShadowVisible: false,
-          // Fondo personalizado con borde inferior
-          headerBackground: () => <HeaderBackgroundWithBorder />,
-        }}
-      />
-
-      <Stack.Screen
-        name="modal-edit-email"
-        options={{
-          title: 'Editar correo',
-          presentation: 'modal',
-          headerShadowVisible: false,
-          // Fondo personalizado con borde inferior
-          headerBackground: () => <HeaderBackgroundWithBorder />,
         }}
       />
     </Stack>
